3c7f89f802263e37623c3e62c6fd3c119eecd815
landeying <44466405+landeying@users.noreply.github.com>
2018-10-31 15:08:55 +0800

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <!--底部菜单栏--> 
+      <footer id="footer">
+        <mt-tabbar>
+			<mt-tab-item  v-for="tbItem in tbItems" id="tbItem">
+				<img slot="icon">
+				<b v-text="tbItem"></b>
+			</mt-tab-item>
+		</mt-tabbar>
+      </footer>
+    </div>
+</template>
+<script>
+      export default{
+        data(){
+            return{
+				tbItems:["书城","频道","发现","我的"]
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
