e5e54f4cf33d0d9eeb16f740468daec93995eefd
landeying <44466405+landeying@users.noreply.github.com>
2018-11-20 19:24:13 +0800

    Add files via upload

--- bookhouse_serve/app.js
+++ bookhouse_serve/app.js
@@ -46,24 +46,166 @@ app.get("/login",(req,res)=>{
   });
 
 app.get("/iconlist",(req,res)=>{
-    var obj = [
-        {id:1,icon_name:"精选",icon_url:"http://127.0.0.1:3000/img/icon/chioceness.png"},
-        {id:2,icon_name:"女生",icon_url:"http://127.0.0.1:3000/img/icon/girl.png"},
-        {id:3,icon_name:"男生",icon_url:"http://127.0.0.1:3000/img/icon/boy.png"},
-        {id:4,icon_name:"免费",icon_url:"http://127.0.0.1:3000/img/icon/free.png"},
-        {id:5,icon_name:"书店",icon_url:"http://127.0.0.1:3000/img/icon/bookstore.png"},
-    ];
-    res.send(obj)
+    var obj = {};
+    var sql = "SELECT fid,fname,fimg FROM chs_family"
+    pool.query(sql,(err,result)=>{
+        if(err) throw err
+        obj = result
+        res.send(obj)
+    })
+})
+//首页分类切换
+app.get("/book",(req,res)=>{
+    var fid = req.query.fid
+    var obj = []
+    var obj1 = []
+    var sql = " SELECT aid,id,subtitle FROM chs_book WHERE fid = ?"
+    var sql1 = "SELECT img FROM chs_pic WHERE fid=?"
+    new Promise(function(open){
+            pool.query(sql,[fid],(err,result)=>{
+                if(err) throw err
+                obj= result;
+                open()
+                console.log("查询book完成")
+            })
+        }).then(function(){
+        new Promise(function(open){
+            pool.query(sql1,[fid],(err,result)=>{
+                if(err) throw err
+                for(var i=0;i<result.length;i++){
+                    for(var j=0;j<obj.length;j++){
+                         if(i==j){
+                             obj1.push(Object.assign(obj[j],result[i]))
+                         }
+                     }
+                   }
+                open()
+                res.send(obj1)
+                console.log("查询图片完成")
+            })
+        })
+    })
 })
 
-app.get("/chimglist",(req,res)=>{
-    var obj = [
-        {id:1,content:"但遇主角，就地为冢",chimg_url:"http://127.0.0.1:3000/img/chome/005QNrjkgy1fl18smrovwj305k07sjt6.jpg"},
-        {id:2,content:"与你朝朝暮暮。",chimg_url:"http://127.0.0.1:3000/img/chome/006E6vgDgy1fteayggaakj30b40fk77w.jpg"},
-        {id:3,content:"治愈他，得到他",chimg_url:"http://127.0.0.1:3000/img/chome/006QYpWhly1fv45dh5f20j316o1kwauv.jpg"},
-        {id:4,content:"重返十七岁",chimg_url:"http://127.0.0.1:3000/img/chome/646d61f8gy1fl5uymg68gj20j60quabj.jpg"},
-        {id:5,content:"回到民国当文豪",chimg_url:"http://127.0.0.1:3000/img/chome/8129d5bdly1fwjki7c95vj205k07s40k.jpg"},
-        {id:6,content:"巧取豪夺狗血文",chimg_url:"http://127.0.0.1:3000/img/chome/ec177d77gy1fw8r884jrnj20b40fk0vc.jpg"},
-    ];
-    res.send(obj)
+//加载更多
+app.get("/getbooklist",(req,res)=>{
+    var pno = req.query.pno //当前页码
+    var pageSize = req.query.pageSize //一页显示几行数据
+    var sql = " SELECT count(id) as c FROM chs_book"//查找总记录数，转换成页数
+    var obj = {}    //保存发送客户端数据
+    var progress = 0  //进度
+    pool.query(sql,(err,result)=>{
+        if(err) throw err
+        var c = Math.ceil(result[0].c/pageSize)
+        obj.pageCount = c
+        progress += 50
+        if(progress == 150){
+            res.send(obj);
+        }
+    })
+    var sql = " SELECT id,bname,subtitle FROM chs_book LIMIT ?,?"
+    var sql1 = "SELECT img FROM chs_pic LIMIT ?,?"
+    var offset = parseInt((pno-1)*pageSize) //计算分页偏移量
+    pageSize = parseInt(pageSize)
+    pool.query(sql,[offset,pageSize],(err,result)=>{
+        if(err) throw err
+        obj.data = result;
+        progress += 50
+        if(progress == 150){
+            res.send(obj);
+        }
+    })
+    pool.query(sql1,[offset,pageSize],(err,result)=>{
+        if(err) throw err
+        obj.img = result
+        progress += 50
+        if(progress == 150){
+            res.send(obj);
+        }
+    })
+    
 })
+//书籍详细信息
+app.get("/getbookinfo",(req,res)=>{
+    var id =req.query.id
+    var obj=[]
+    var obj1 = []
+    var sql = " SELECT * FROM chs_book WHERE id=?"
+    var sql1 = "SELECT img FROM chs_pic WHERE id=?"
+     new Promise(function(open){
+        pool.query(sql,[id],(err,result)=>{
+            if(err) throw err
+            obj= result;
+            open()
+            console.log("查询book完成")
+            })
+        }).then(function(){
+        new Promise(function(open){
+            pool.query(sql1,[id],(err,result)=>{
+                if(err) throw err
+                for(var i=0;i<result.length;i++){
+                    for(var j=0;j<obj.length;j++){
+                         if(i==j){
+                             obj1.push(Object.assign(obj[j],result[i]))
+                         }
+                     }
+                   }
+                open()
+                res.send(obj1)
+                console.log("查询图片完成")
+            })
+        })
+    })
+})
+//发表评论
+const qs = require("querystring");
+app.post("/postcomment",(req,res)=>{
+    req.on("data",(buf)=>{
+        var str = buf.toString();  //1:将参数转字符串
+        var obj = JSON.parse(str); //2:将参数转对象
+        var msg = obj.msg;         
+        var uid = parseInt(obj.uid);
+        console.log(obj.nid)
+        var sql = "INSERT INTO chs_comment(content,uname,ctime,uid) VALUES(?,'匿名',now(),?)";
+        pool.query(sql,[msg,uid],(err,result)=>{
+            if(err)throw err;
+             res.send({code:1,msg:"添加成功"});
+     })
+    })
+})
+//用户获取指定编号所有评论
+//分页显示
+app.get("/booklist",(req,res)=>{
+    var sql = " SELECT id,bname,subtitle FROM chs_book LIMIT ?,?"
+    var offset = parseInt((pno-1)*pageSize) //计算分页偏移量
+    pageSize = parseInt(pageSize)
+    pool.query(sql,[offset,pageSize],(err,result)=>{
+        if(err) throw err
+        obj.data = result
+        progress += 50
+        if(progress == 100){
+            res.send(obj);
+        }
+    })
+})
+ //分区显示
+ app.get("/area",(req,res)=>{
+     var area = {}
+     var sql = "SELECT aid,aname FROM chs_area_family"
+     pool.query(sql,(err,result)=>{
+         if(err) throw err
+         area = result
+         res.send(area)
+     })  
+ }) 
+ //搜索排行
+ app.get("/search",(req,res)=>{
+     var obj = []
+     var sql = "SELECT id,bname FROM chs_book"
+     pool.query(sql,(err,result)=>{
+         if(err) throw err
+         obj = result
+         res.send(obj)
+     })
+ })
+
